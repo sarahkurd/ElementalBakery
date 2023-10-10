4e4cb73b1643e5db6496179d4c6787adcf92d43e
Sarah Kurdoghlian <sarahkurdoghlian@Sarahs-MBP-2.lan>
2023-10-09 22:32:01 -0700

    make sprite rotate on left right input

--- Assets/Scripts/PlayerMovementDevelopment.cs
+++ Assets/Scripts/PlayerMovementDevelopment.cs
@@ -8,19 +8,27 @@ public class PlayerMovementDevelopment : MonoBehaviour
 {
     private Rigidbody2D rb;
     private BoxCollider2D boxCollider;
-    private SpriteRenderer sprite;
+    private SpriteRenderer spriteRenderer;
     [SerializeField] private bool isDescending;
     [SerializeField] private LayerMask jumpableGround;
     [SerializeField] private LayerMask breakableGround;
-    [SerializeField] private LayerMask breakableGroundStageTwo;
-    
+    [SerializeField] private Sprite powerLeft;
+    [SerializeField] private Sprite powerBottom;
+    [SerializeField] private Sprite powerRight;
+    [SerializeField] private Sprite powerTop;
+    private Sprite currentSprite;
     private enum MovementState { idle, running, jumping, falling }
+
+    private List<Sprite> spriteOrder;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        currentSprite = spriteRenderer.sprite;
+        spriteOrder = new List<Sprite>() { powerRight, powerBottom, powerLeft, powerTop };
     }
 
     // Update is called once per frame
@@ -36,6 +44,39 @@ public class PlayerMovementDevelopment : MonoBehaviour
             rb.velocity = new Vector2(0, 7);
         }
         
+        // rotate player mechanics
+        bool leftKeyInput = Input.GetKeyUp("left");
+        bool rightKeyInput = Input.GetKeyUp("right");
+        setCurrentSprite(leftKeyInput, rightKeyInput);
+        spriteRenderer.sprite = currentSprite; // update the sprite in SpriteRenderer component
+    }
+
+    private void setCurrentSprite(bool leftKeyInput, bool rightKeyInput)
+    {
+        if (leftKeyInput)
+        {
+            int index = spriteOrder.IndexOf(currentSprite);
+            if (index == 0)
+            {
+                currentSprite = spriteOrder[spriteOrder.Count - 1];
+            }
+            else
+            {
+                currentSprite = spriteOrder[index - 1];
+            }
+        }
+        else if (rightKeyInput)
+        {
+            int index = spriteOrder.IndexOf(currentSprite);
+            if (index == spriteOrder.Count - 1)
+            {
+                currentSprite = spriteOrder[0];
+            }
+            else
+            {
+                currentSprite = spriteOrder[index + 1];
+            }
+        }
     }
 
     private bool IsGrounded()
@@ -60,7 +101,6 @@ public class PlayerMovementDevelopment : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other)
     {
         bool isBreakableLayer = other.gameObject.layer == LayerMask.NameToLayer("Breakable");
-        Debug.Log(isBreakableLayer);
         if (isBreakableLayer)
         {
             Destroy(other.gameObject);
