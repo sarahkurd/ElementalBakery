31c5ef6b9ce47226a48b975b268e6eccc9868fd5
tanvibhaskarwar <tanvi.bhaskarwar@gmail.com>
2023-10-10 12:59:49 -0700

    Added ingredient and destroy

--- Assets/Scripts/PlayerMovementDevelopment.cs
+++ Assets/Scripts/PlayerMovementDevelopment.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class PlayerMovementDevelopment : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public float jumpForce = 4f;
     private BoxCollider2D boxCollider;
     private SpriteRenderer spriteRenderer;
     [SerializeField] private bool isDescending;
@@ -17,10 +18,16 @@ public class PlayerMovementDevelopment : MonoBehaviour
     [SerializeField] private Sprite powerRight;
     [SerializeField] private Sprite powerTop;
     private Sprite currentSprite;
-    private enum MovementState { idle, running, jumping, falling }
+    private enum MovementState { idle, running, jumping, falling };
+    public float moveSpeed = 10f;
+    private bool isOnObject = false;
+    private bool wasGrounded = true;
+
 
     private List<Sprite> spriteOrder;
-    
+    private float timer = 0f;
+    public float destroyTime = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,47 +41,56 @@ public class PlayerMovementDevelopment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool currentlyGrounded = IsGrounded();
         // horizontal mechanics
         float horizontalInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(7f * horizontalInput, rb.velocity.y);
-        
+        transform.position += new Vector3(horizontalInput, 0, 0) * moveSpeed * Time.deltaTime;
+
         // vertical jump mechanics
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)) && IsGrounded())
         {
-            rb.velocity = new Vector2(0, 7);
+            Jump();
         }
-        
+
         // rotate player mechanics
-        bool leftKeyInput = Input.GetKeyUp("left");
-        bool rightKeyInput = Input.GetKeyUp("right");
-        setCurrentSprite(leftKeyInput, rightKeyInput);
+        setCurrentSprite();
         spriteRenderer.sprite = currentSprite; // update the sprite in SpriteRenderer component
+
+        //timer if player lands on ingredient
+        wasGrounded = currentlyGrounded;
+
     }
 
-    private void setCurrentSprite(bool leftKeyInput, bool rightKeyInput)
+    void Jump()
     {
-        if (leftKeyInput)
-        {
-            int index = spriteOrder.IndexOf(currentSprite);
-            if (index == 0)
+        rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+    }
+
+    private void setCurrentSprite()
+    {   if (!IsGrounded()){
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                currentSprite = spriteOrder[spriteOrder.Count - 1];
+                int index = spriteOrder.IndexOf(currentSprite);
+                if (index == 0)
+                {
+                    currentSprite = spriteOrder[spriteOrder.Count - 1];
+                }
+                else
+                {
+                    currentSprite = spriteOrder[index - 1];
+                }
             }
-            else
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                currentSprite = spriteOrder[index - 1];
-            }
-        }
-        else if (rightKeyInput)
-        {
-            int index = spriteOrder.IndexOf(currentSprite);
-            if (index == spriteOrder.Count - 1)
-            {
-                currentSprite = spriteOrder[0];
-            }
-            else
-            {
-                currentSprite = spriteOrder[index + 1];
+                int index = spriteOrder.IndexOf(currentSprite);
+                if (index == spriteOrder.Count - 1)
+                {
+                    currentSprite = spriteOrder[0];
+                }
+                else
+                {
+                    currentSprite = spriteOrder[index + 1];
+                }
             }
         }
     }
@@ -105,5 +121,25 @@ public class PlayerMovementDevelopment : MonoBehaviour
         {
             Destroy(other.gameObject);
         }
+
+        if (other.gameObject.CompareTag("Ingredient"))
+        {
+            isOnObject = true;
+            if (currentSprite == powerBottom)
+            {
+                Destroy(other.gameObject, 2);
+            }
+           
+        }
     }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ingredient")) 
+        {
+            isOnObject = false;
+            timer = 0f; 
+        }
+    }
+
 }
