b2054dfd4eb9744a6c91766d307f98724b139cc7
11! <yiyiyuan@usc.edu>
2023-10-28 01:28:56 -0700

    Move Timer Script to the Scripts folder

--- Assets/Scripts/PlayerRanking.cs
+++ Assets/Scripts/PlayerRanking.cs
@@ -10,17 +10,18 @@ public class PlayerRanking : MonoBehaviour
 
     // Define rank thresholds (in seconds)
     private float rankSThreshold = 60f;
-    private float rankAThreshold = 120f;
-    private float rankBThreshold = 180f;
+    private float rankAThreshold = 100f;
+    private float rankBThreshold = 120f;
 
     public void CalculateRank()
     {
         float completionTime = gameTimer.ElapsedTime;
+        Debug.Log("Completion Time: " + completionTime);
 
-        string rank = "C";
-        if (completionTime <= rankSThreshold) rank = "S";
-        else if (completionTime <= rankAThreshold) rank = "A";
-        else if (completionTime <= rankBThreshold) rank = "B";
+        string rank = "You meow out of time";
+        if (completionTime <= rankSThreshold) rank = "Master Chef!!!";
+        else if (completionTime <= rankAThreshold) rank = "Great Chef!!";
+        else if (completionTime <= rankBThreshold) rank = "Novice Chef!";
 
         rankText.text = "Rank: " + rank;
     }
