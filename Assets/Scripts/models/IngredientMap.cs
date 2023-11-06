2d46f4121ba4dcd0fa000ef2d665b760653c3248
Sarah Kurdoghlian <sarahkurdoghlian@Sarahs-MBP-2.lan>
2023-11-06 02:39:42 -0800

    add  timer to all levels. Player can drop items on stove/siunk station. Player can pick up from stove/sink statio

--- Assets/Scripts/models/IngredientMap.cs
+++ Assets/Scripts/models/IngredientMap.cs
@@ -6,13 +6,13 @@ namespace models
     {
         public static Dictionary<string, Ingredient> dict = new Dictionary<string, Ingredient>()
         {
-            { "chicken", new Ingredient("chicken", 10.0f, CookType.FIRE) },
+            { "chicken", new Ingredient("chicken", 15.0f, CookType.FIRE) },
             { "toast", new Ingredient("toast", 2.0f, CookType.FIRE) },
-            { "steak", new Ingredient("steak", 6.0f, CookType.FIRE) },
-            { "bacon", new Ingredient("bacon", 5.0f, CookType.FIRE) },
+            { "steak", new Ingredient("steak", 10.0f, CookType.FIRE) },
+            { "bacon", new Ingredient("bacon", 8.0f, CookType.FIRE) },
             { "croissant", new Ingredient("croissant", 5.0f, CookType.FIRE) },
-            { "tomato", new Ingredient("tomato",3.0f, CookType.WATER) },
-            { "lettuce", new Ingredient("lettuce", 3.0f, CookType.WATER) },
+            { "tomato", new Ingredient("tomato",5.0f, CookType.WATER) },
+            { "lettuce", new Ingredient("lettuce", 5.0f, CookType.WATER) },
             { "blueberry", new Ingredient("blueberry", 3.0f, CookType.WATER) },
             { "soup", new Ingredient("soup", 5.0f, CookType.AIR) },
         };
