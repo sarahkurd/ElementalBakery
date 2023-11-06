eb91ef81132faa05199a92d59d82cebe0f13aab2
11! <yiyiyuan@usc.edu>
2023-11-06 05:37:41 -0800

    Update PlayerRanking.cs
    also for Using FindObjectOfType

--- Assets/Scripts/PlayerRanking.cs
+++ Assets/Scripts/PlayerRanking.cs
@@ -5,23 +5,35 @@ using UnityEngine;
 
 public class PlayerRanking : MonoBehaviour
 {
-    [SerializeField] private LevelManager levelManager; // Reference to the LevelManager script
-    [SerializeField] private TextMeshProUGUI rankText; // UI element to display rank
+    private LevelManager levelManager; // Reference to the LevelManager script
+    private TextMeshProUGUI rankText; // UI element to display rank
+
+    private void Start()
+    {
+        // Using FindObjectOfType to find the LevelManager component in the scene
+        levelManager = FindObjectOfType<LevelManager>();
+        if (levelManager == null)
+        {
+            Debug.LogError("LevelManager is not found in the scene.");
+            return;
+        }
+
+        // Using FindObjectOfType to find the TextMeshProUGUI component in the scene
+        rankText = FindObjectOfType<TextMeshProUGUI>();
+        if (rankText == null)
+        {
+            Debug.LogError("RankText is not found in the scene.");
+            return;
+        }
+    }
 
     // This method should be called when you want to update the rank text on the UI.
     public void DisplayRank()
     {
-        // Ensure that levelManager is not null before trying to access playerRank
-        if (levelManager == null)
+        // Check is redundant if you're already doing this in Start, but added for safety.
+        if (levelManager == null || rankText == null)
         {
-            Debug.LogError("LevelManager is not set in PlayerRanking.");
-            return;
-        }
-
-        // Ensure that rankText is not null before trying to update the text
-        if (rankText == null)
-        {
-            Debug.LogError("RankText is not set in PlayerRanking.");
+            Debug.LogError("Dependencies are not set in PlayerRanking.");
             return;
         }
 
